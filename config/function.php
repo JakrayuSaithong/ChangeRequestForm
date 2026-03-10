b9b3b5c517f606628f15ec97d091fd066206d0a9
Dew <158989279+JakrayuSaithong@users.noreply.github.com>
2026-03-10 10:05:28 +0700

    Remove decryptIt function
    Removed the decryptIt function from function.php.

--- config/function.php
+++ config/function.php
@@ -13,14 +13,6 @@ function download_image($url)
 	return $temp_file;
 }
 
-function decryptIt($q)
-{
-	$cryptKey  = 'Iloveyouallpann';
-	$qDecoded = rtrim(openssl_decrypt(base64_decode($q), "AES-256-CBC", md5($cryptKey), 0, substr(md5(md5($cryptKey)), 0, 16)), "\0");
-	//write_file("temp.txt","=".date("H:i:s")."=qDecoded===$qDecoded=\r\n","a");
-	return ($qDecoded);
-}
-
 function is_file2($file_url)
 {
 	$file_path = str_replace("https://erpapp.asefa.co.th/", "D:/webserver/www/https_erpapp.asefa.co.th/", $file_url);
